6f453749086e66e757de98ecc7900dff526d121c
Reuben Crimp <reubencrimp@gmail.com>
2014-05-13 03:46:44 +1200

    updated

--- ass2/Nqueen.java
+++ ass2/Nqueen.java
@@ -3,9 +3,11 @@ import java.lang.Math;
      
 public class Nqueen{
    public static final int BOARD_SIZE = 8;
-   public static final int POPULATION = 100;
+   static final double MUTATION_P = 0.1;
+   /* must be a multiple of 4 if using breedEveryEven */
+   public static final int POPULATION = 100; 
+
    public static int MaxAttackingPairs;
-   static final double MUTATION_P = 0.01;
        
    public static void main(String[] args){  
       MaxAttackingPairs = binomial(BOARD_SIZE, 2);
@@ -16,33 +18,24 @@ public class Nqueen{
       for(int i = 0; i < POPULATION; i++)
          boards.add(new Board());
       
-      sort(boards);
-      while(boards.get(0).fitness > 0 && gen < 10000)
+      while(findBest(boards).fitness > 0 && gen < 1000000)
          {
             //shuffle(boards);
-            breedEveryPair(boards);
             sort(boards);
+
+            //breedRandomPair(boards);
+            breedEveryNew(boards);
+            
+            //breedEveryPair(boards);
+            
             gen++;
          }
       
-      System.out.println(boards.get(0));
+      System.out.println(findBest(boards));
       System.out.println("generations: " + gen);     
    }
 
-   private static int binomial(int n, int k){
-      return factorial(n) / (factorial(k)*factorial(n-k));
-   }
-   private static int factorial(int n){
-      if (n < 0) {
-         System.err.println("err:  negative factorial");
-         return 0;
-      }
-      if (n < 2) return 1;
-      return n * factorial(n-1);
-   }
-
    public static void breedEveryPair(List<Board> boards){
-      int added = 0;
       for(int i = 0; i < POPULATION/2; i+=2){
          Board[] c = crossover_split(boards.get(i), boards.get(i+1));
          //Board[] c = crossover_uniform(boards.get(i), boards.get(i+1));
@@ -50,24 +43,39 @@ public class Nqueen{
          // boards.set(POPULATION/2 + i + 1, c[1]);
          boards.set(POPULATION - i - 1, c[0]);
          boards.set(POPULATION - i - 2, c[1]);
-         added += 2;
+      }
+   }
+   public static void breedEverySecondPair(List<Board> boards){
+      for(int i = 0; i < POPULATION; i+=4){
+         Board[] c = crossover_split(boards.get(i), boards.get(i+2));;
+         boards.set(i+1, c[0]);
+         boards.set(i+3, c[1]);
+      }
+   }
+   public static void breedRandomPair(List<Board> boards){
+      for(int i = 0; i < POPULATION; i+=2){
+         Board[] c = crossover_split(boards.get((int)(Math.random() * POPULATION)), boards.get((int)(Math.random() * POPULATION)));;
+         boards.set((int)(Math.random() * POPULATION), c[0]);
+         boards.set((int)(Math.random() * POPULATION), c[1]);
+      }
+   }
+   public static void breedEveryNew(List<Board> boards){
+      for(int i = 0; i < POPULATION/4; i++){
+         Board[] c = crossover_split(boards.get(i), boards.get(POPULATION/4+i));
+         boards.set(POPULATION/2 + i, c[0]);
+         boards.set((int)(POPULATION*(0.75)) + i, c[1]);
       }
    }
 
-  public static Board[] crossover_split(Board parent1, Board parent2){
+   public static Board[] crossover_split(Board parent1, Board parent2){
       Board[] children = new Board[2];
       //int splitPoint = (int)(Math.random() * (BOARD_SIZE-1));
-      double temp =
-      Math.min(parent1.fitness, parent2.fitness) /
-       (double)Math.max(parent1.fitness, parent2.fitness); 
-      //System.out.println(parent1.fitness + " " + parent2.fitness + " " +  temp);
-      int splitPoint = (int)(temp * 7);
-      
+      int splitPoint = (int)((Math.min(parent1.fitness, parent2.fitness) /
+                              (double)Math.max(parent1.fitness, parent2.fitness)) * 7);
       children[0] = new Board(parent1, parent2, splitPoint);
       children[1] = new Board(parent2, parent1, splitPoint);
       return children;
    }
-
    public static Board[] crossover_uniform(Board parent1, Board parent2){
       Board[] children = new Board[2];
       children[0] = new Board(parent1, parent2);
@@ -75,12 +83,12 @@ public class Nqueen{
       return children;
    }
 
-   public static Board[] crossover_three(Board parent1, Board parent2, Board parent3){
-      Board[] children = new Board[3];
-      children[0] = new Board(parent1, parent2, parent3);
-      children[1] = new Board(parent1, parent3, parent2);
-      children[2] = new Board(parent2, parent3, parent1);
-      return children;
+   public static Board findBest(List<Board> boards){
+      Board best = boards.get(0);
+      for(int i = 1; i < POPULATION; i+=1)
+         if(boards.get(i).fitness < best.fitness)
+            best = boards.get(i);
+      return best;
    }
      
    public static void shuffle(List<Board> list){
@@ -92,26 +100,36 @@ public class Nqueen{
          list.set(i, temp);
       }
    }
-
    public static void sort(List<Board> list){
       Collections.sort(list, new Comparator<Board>(){
-         @Override
-            public int compare(Board v1, Board v2) {
-            return v1.compareTo(v2);
-         }
-      });
+            @Override
+               public int compare(Board v1, Board v2) {
+               return v1.compareTo(v2);
+            }
+         });
+   }
+
+   /* the maximum number of attacking queens is computed from the follwing methods */
+   private static int binomial(int n, int k){
+      return factorial(n) / (factorial(k)*factorial(n-k));
+   }
+   private static int factorial(int n){
+      if (n < 0) {
+         System.err.println("err:  negative factorial");
+         return 0;
+      }
+      if (n < 2) return 1;
+      return n * factorial(n-1);
    }
 }
      
 class Board {
+   /* the particular board permutation is stored in aptly named array */
    public int[] array;
    public double fitness;
        
    public Board(){
       array = new int[Nqueen.BOARD_SIZE];
-      /*for(int i = 0; i < size; i++)
-        array[i] = (int)(Math.random() * size);
-        mutate();*/
       for(int i = 0; i < Nqueen.BOARD_SIZE; i++)
          array[i] = i; //array = {1, 2, 3, 4 ... }
       shuffle();
@@ -125,26 +143,13 @@ class Board {
       double p = best.fitness / (worst.fitness + best.fitness);
       
       for(int i = 0; i < Nqueen.BOARD_SIZE; i++){
-            if(Math.random() < p)
-               array[i] = best.array[i];
-            else
-               array[i] = worst.array[i];
-         }
+         if(Math.random() < p)
+            array[i] = best.array[i];
+         else
+            array[i] = worst.array[i];
+      }
       mutate();
       this.fitness = calcFitness();
-   }
-
-   public Board(Board parent1, Board parent2, Board parent3){
-      array = new int[Nqueen.BOARD_SIZE];
-      
-      for(int i = 0; i < Nqueen.BOARD_SIZE; i++){
-            if (parent1.array[i] == parent2.array[i])
-               array[i] = parent1.array[i];
-            else
-               array[i] = parent3.array[i];
-         }
-      mutate();
-      fitness = calcFitness();
    }
    
    public Board(Board parent1, Board parent2, int splitPoint){
@@ -161,20 +166,9 @@ class Board {
      
    private void mutate(){
       if(Math.random() < Nqueen.MUTATION_P){
-         int a1 = (int)(Math.random() * Nqueen.BOARD_SIZE);
-         int a2 = (int)(Math.random() * (Nqueen.BOARD_SIZE-1));
-         if(a1 == a2) a2++;
-         //swap indeces a1 and a2
-         int tmp = array[a1];
-         array[a1] = array[a2];
-         array[a2] = tmp;
+         array[(int)(Math.random() * Nqueen.BOARD_SIZE)] =
+            (int)(Math.random() * Nqueen.BOARD_SIZE);
       }
-      
-      // if(Math.random() < MUTATION_P)
-      // {
-      //    int tmp = (int)(Math.random() * Nqueen.BOARD_SIZE);
-      //    array[tmp] = (int)(Math.random() * Nqueen.BOARD_SIZE);
-      //    }
    }
        
    /* smaller fitness is better */
@@ -182,7 +176,9 @@ class Board {
       int attackingPairs = 0;
       for(int i = 0; i < Nqueen.BOARD_SIZE; i++)
          attackingPairs += countEast(i) + countSouthEast(i) + countNorthEast(i);
-      return attackingPairs;
+      if (attackingPairs == 0) return 0;
+      if (attackingPairs == Nqueen.MaxAttackingPairs) return 1;
+      return 1 / (double)(Nqueen.MaxAttackingPairs - attackingPairs);
    }
    
    //count the queens horizontally left (east) of the current piece
